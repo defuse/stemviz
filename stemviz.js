6540ccbb5325554d508ea09368653deae365c230
Taylor Hornby <taylor@defuse.ca>
2018-12-25 17:14:06 -0700

    add spectrum

--- stemviz.js
+++ stemviz.js
@@ -2,8 +2,7 @@ const { createCanvas, loadImage } = require('canvas');
 const canvas = createCanvas(1920, 1080);
 const ctx = canvas.getContext('2d');
 const WaveFile = require('wavefile');
-
-// ffmpeg -r 30 -f image2 -s 1920x1080 -i frames/%05d.png -i test.wav -vcodec libx264 -crf 25 -pix_fmt yuv420p circle.mp4
+var ft = require('fourier-transform');
 
 // TODO: add option to just play the animation in realtime w/ audio
 
@@ -28,33 +27,33 @@ function load_wav(path, fps) {
         }
     };
 
-    /* Peak TODO */
-    wav_computed.peak_at = function(time_s) {
-        var frame = Math.floor(time_s * this.fps);
-        if (frame < this.data.rms.length) {
-            return this.data.peak[frame];
-        } else {
-            return 0.0;
-        }
-    };
-
-    /* Moving average RMS TODO */
+    /* Moving average RMS */
     wav_computed.avg_rms_at = function(time_s) {
         var frame = Math.floor(time_s * this.fps);
-        if (frame < this.data.rms.length) {
+        if (frame < this.data.avg_rms.length) {
             return this.data.avg_rms[frame];
         } else {
             return 0.0;
         }
     };
 
-    /* Pitch tracker TODO*/
-    wav_computed.pitch_at = function(time_s) {
+    /* Peak TODO */
+    wav_computed.peak_at = function(time_s) {
         var frame = Math.floor(time_s * this.fps);
-        if (frame < this.data.rms.length) {
-            return this.data.pitch[frame];
+        if (frame < this.data.peak.length) {
+            return this.data.peak[frame];
         } else {
             return 0.0;
+        }
+    };
+
+    /* Spectra */
+    wav_computed.spectra_at = function(time_s) {
+        var frame = Math.floor(time_s * this.fps);
+        if (frame < this.data.spectra.length) {
+            return this.data.spectra[frame];
+        } else {
+            return new Array(512).fill(0.0);
         }
     };
 
@@ -81,10 +80,11 @@ function load_wav(path, fps) {
     var length_s = wav.data.samples.length / sample_rate / 2 / 2;
     var total_frames = Math.ceil(length_s * fps);
 
-    wav_computed.data.rms = new Array(total_frames);
-
     console.log("Preprocessing RMS levels...");
 
+    wav_computed.data.rms = new Array(total_frames);
+
+frameloop:
     for (var frame = 0; frame < total_frames; frame++) {
         var time = frame / fps;
 
@@ -100,7 +100,7 @@ function load_wav(path, fps) {
                 var sample = wav.getSample(start_sample + s) / 32768;
             } catch (e) {
                 wav_computed.data.rms[frame] = 0.0;
-                break;
+                break frameloop;
             }
             mean_squared += sample * sample;
         }
@@ -116,6 +116,71 @@ function load_wav(path, fps) {
         wav_computed.data.rms[frame] /= max_rms;
     }
 
+    console.log("Preprocessing averaged RMS levels...");
+
+    // Compute moving average RMS.
+    wav_computed.data.avg_rms = new Array(total_frames);
+
+    var AVG_WINDOW_FRAMES = 6;
+    for (var frame = 0; frame < total_frames; frame++) {
+        var avg_pos = Math.max(0, frame - AVG_WINDOW_FRAMES + 1);
+        var sum = 0.0;
+        var n = 0;
+        for (; avg_pos <= frame; avg_pos++) {
+            sum += wav_computed.data.rms[avg_pos];
+            n++;
+        }
+        wav_computed.data.avg_rms[frame] = sum/n;
+    }
+
+    // Normalize the RMS signal.
+    var max_avg_rms = Math.max.apply(null, wav_computed.data.avg_rms);
+    for (var frame = 0; frame < total_frames; frame++) {
+        // FIXME: this will crash on an all-silence file
+        wav_computed.data.avg_rms[frame] /= max_avg_rms;
+    }
+
+    console.log("Preprocessing spectra...");
+
+    wav_computed.data.spectra = new Array(total_frames);
+
+    var spectra_max = 0.0;
+framelooptwo:
+    for (var frame = 0; frame < total_frames; frame++) {
+        var time = frame / fps;
+
+        // Compute RMS.
+        var BUFFER_SAMPLES = 2048;
+        var start_sample = Math.max(0, Math.floor(2 * time * sample_rate - BUFFER_SAMPLES/2.0));
+        var mean_squared = 0.0;
+
+        var mono_sum = new Array(BUFFER_SAMPLES / 2);
+
+        for (var s = 0; s < BUFFER_SAMPLES / 2; s++) {
+            try {
+                // FIXME: do this the correct way.
+                var s1 = wav.getSample(start_sample + 2 * s) / 32768;
+                var s2 = wav.getSample(start_sample + 2 * s + 1) / 32768;
+                mono_sum[s] = (s1 + s2) / 2;
+            } catch (e) {
+                // FIXME: compute the 512 properly
+                wav_computed.data.spectra[frame] = new Array(512).fill(0.0);
+                break framelooptwo;
+            }
+        }
+
+        wav_computed.data.spectra[frame] = ft(mono_sum);
+        spectra_max = Math.max(spectra_max, Math.max.apply(null, wav_computed.data.spectra[frame]));
+    }
+
+    // Globally normalize the spectra.
+    // FIXME: is this really what i want to do?
+    for (var frame = 0; frame < total_frames; frame++) {
+        for (var i = 0; i < wav_computed.data.spectra[frame].length; i++) {
+            wav_computed.data.spectra[frame][i] /= spectra_max;
+        }
+    }
+
     console.log("Finished preprocessing.");
 
     console.log("Writing to the cache...");
@@ -124,17 +189,25 @@ function load_wav(path, fps) {
     return wav_computed;
 }
 
-function draw_bar_chart(canvas, ctx, elements) {
-    var BAR_WIDTH = Math.floor(canvas.width / elements.length);
+function draw_spectra_bars(canvas, ctx, spectra) {
+    // 1080 is divisible by 90
+    var truncated = spectra.slice(0, 90);
 
-    for (var i = 0; i < elements.length; i++) {
-        var element = elements[i];
-        ctx.fillStyle = "#00aacc";
+    var BAR_HEIGHT = Math.floor(canvas.height / truncated.length);
+
+    for (var i = 0; i < truncated.length; i++) {
+        ctx.fillStyle = "#ffeb3d";
         ctx.fillRect(
-            BAR_WIDTH * i,
-            canvas.height - element.rms_at(time_s) * canvas.height,
-            BAR_WIDTH,
-            element.rms_at(time_s) * canvas.height
+            0,
+            BAR_HEIGHT * i,
+            truncated[i] * canvas.width,
+            BAR_HEIGHT
+        );
+        ctx.fillRect(
+            canvas.width - truncated[truncated.length - i - 1] * canvas.width,
+            BAR_HEIGHT * i,
+            truncated[truncated.length - i - 1] * canvas.width,
+            BAR_HEIGHT
         );
     }
 }
@@ -184,7 +257,7 @@ function draw_inner_circle(canvas, ctx, color, value) {
     ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
     ctx.fillStyle = color;
     ctx.fill();
-    ctx.lineWidth = 4;
+    ctx.lineWidth = 2;
     ctx.strokeStyle = '#ffffff';
     ctx.stroke();
 }
@@ -219,6 +292,7 @@ function draw_faller_bars(canvas, ctx, color, value) {
     }
 }
 
+var master = load_wav("./Mix21StemsNormalized/ABCDEF-Mix021.wav", FRAMES_PER_SECOND);
 var airy_chords = load_wav("./Mix21StemsNormalized/ABCDEF-Mix021 Airy Chords.wav", FRAMES_PER_SECOND);
 var verse_melody = load_wav("./Mix21StemsNormalized/ABCDEF-Mix021 Verse Melody.wav", FRAMES_PER_SECOND);
 var snare_fill = load_wav("./Mix21StemsNormalized/ABCDEF-Mix021 Snare Fill.wav", FRAMES_PER_SECOND);
@@ -246,6 +320,7 @@ var time_s = 0.0;
 console.log("Drawing video frames...");
 
 var outer_circle_color = "#4d1b7b";
+var inner_circle_color = "#ef4f91";
 var BPM = 140;
 
 // 226
@@ -258,26 +333,20 @@ for (var frame = 0; frame < 226 * FRAMES_PER_SECOND; frame++) {
     ctx.fillStyle = "#000000";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    //var elements = [
-    //    airy_chords, verse_melody, snare_fill, rev_snap, verse_snap, rev_crash,
-    //    funky_bongo, fast_bongo, verse_snare, downlifter, build_arp, build_snare,
-    //    vox_riser, drop_kick, drop_snare, hard_ssw, drop_fx
-    //];
-    //
-    //draw_bar_chart(canvas, ctx, elements);
-
     /* Background Color */
-    draw_bg_color(canvas, ctx, "#363b74", airy_chords.rms_at(time_s));
+    draw_bg_color(canvas, ctx, "#363b74", airy_chords.avg_rms_at(time_s));
     draw_bg_color(canvas, ctx, "#ffffff", hard_ssw.rms_at(time_s));
+
 
     /* Riser Bars */
     draw_riser_bars(
         canvas, ctx, "#ef4f91",
         Math.max(
-            rev_crash.rms_at(time_s),
-            drop_fx.rms_at(time_s),
-            funky_bongo.rms_at(time_s),
-            vox_riser.rms_at(time_s)
+            snare_fill.avg_rms_at(time_s),
+            rev_crash.avg_rms_at(time_s),
+            drop_fx.avg_rms_at(time_s),
+            funky_bongo.avg_rms_at(time_s),
+            vox_riser.avg_rms_at(time_s)
         )
     );
 
@@ -285,17 +354,23 @@ for (var frame = 0; frame < 226 * FRAMES_PER_SECOND; frame++) {
     draw_faller_bars(
         canvas, ctx, "#673888",
         Math.max(
-            snare_fill.rms_at(time_s),
-            downlifter.rms_at(time_s),
-            fast_bongo.rms_at(time_s),
-            rev_crash.rms_at(time_s)
+            snare_fill.avg_rms_at(time_s),
+            downlifter.avg_rms_at(time_s),
+            fast_bongo.avg_rms_at(time_s),
+            rev_crash.avg_rms_at(time_s),
+            vox_riser.avg_rms_at(time_s)
         )
     );
 
+    /* Spectra along the side. */
+    draw_spectra_bars(canvas, ctx, master.spectra_at(time_s));
+
     if (drop_kick.rms_at(time_s) > 0) {
-        outer_circle_color = "#e80000"
+        outer_circle_color = "#e80000";
+        inner_circle_color = "#000000";
     } else if (verse_kick.rms_at(time_s) > 0 || verse_kick_2.rms_at(time_s) > 0) {
-        outer_circle_color = "#4d1b7b"
+        outer_circle_color = "#4d1b7b";
+        inner_circle_color = "#ef4f91";
     }
 
     /* Outer Circle */
@@ -311,7 +386,7 @@ for (var frame = 0; frame < 226 * FRAMES_PER_SECOND; frame++) {
 
     /* Inner Circle */
     draw_inner_circle(
-        canvas, ctx, "#ef4f91",
+        canvas, ctx, inner_circle_color,
         Math.max(
             0.50 * rev_snap.rms_at(time_s),
             build_snare.rms_at(time_s),
